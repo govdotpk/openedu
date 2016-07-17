0eb0bb3039aa97af02abc8f420fcd1ce9e772f38
Shaji Ahmed <schajee@gmail.com>
2016-07-17 13:15:13 +0500

    Added style and comment form

--- feedback/resources/views/feedback.blade.php
+++ feedback/resources/views/feedback.blade.php
@@ -1,18 +1,38 @@
-<table>
-	<thead>
-		<th>ID</th>
-		<th>From</th>
-		<th>Time</th>
-		<th>Message</th>
-	</thead>
-	<tbody>
-@foreach ($feedback as $f)
-	<tr>
-		<td>{{$f->id}}</td>
-		<td>{{$f->sender_num}}</td>
-		<td>{{date('j F Y', $f->time)}}</td>
-		<td>{{$f->text}}</td>
-	</tr>
-@endforeach
-</tbody>
-</table>
+@extends('layouts.main')
+
+@section ('content')
+<section>
+    <div class="container">
+
+		<table class="table table-striped">
+			<thead>
+				<th>ID</th>
+				<th>From</th>
+				<th>Time</th>
+				<th>Message</th>
+				<th>Comment</th>
+			</thead>
+			<tbody>
+		@foreach ($feedback as $f)
+			<tr>
+				<td>{{$f->id}}</td>
+				<td>{{$f->sender_num}}</td>
+				<td>{{date('j F Y', $f->time)}}</td>
+				<td>{{$f->text}}</td>
+				<td>
+					<form class="form-inline">
+						<div class="form-group">
+							<input type="text" class="form-control" placeholder="">
+						</div>
+						<div class="form-group">
+							<button type="submit" class="btn btn-default">Comment</button>
+						</div>
+					</form>
+				</td>
+			</tr>
+		@endforeach
+		</tbody>
+		</table>
+	</div>
+</section>
+@endsection
---
+++ feedback/resources/views/partials/header.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <title>Aao Sochen</title>
+        <link rel="shortcut icon" href="/favicon.ico">
+        <link href="/assets/styles/bootstrap.min.css" rel="stylesheet">
+        <link href="/assets/styles/font-awesome.min.css" rel="stylesheet">
+        <link href="/assets/styles/app.css" rel="stylesheet">    
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <header>
+            <nav class="navbar navbar-default">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="/">Aao Sochen</a>
+                    </div>
+
+                </div>
+            </nav>
+        </header>
